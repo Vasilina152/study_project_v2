765c8ac0cc1f253aef483177e573dcee01befdde
Alexey Shirokov <shirokovam@lad24.ru>
2021-05-15 10:08:23 +0300

    add component and containers

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import MainPage from "./Components/MainPage";
-
-function App() {
-  return <MainPage />;
-}
-
-export default App;
---
+++ src/Containers/App/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainPageContainer from "../MainPageContainer";
+
+function App() {
+  return <MainPageContainer />;
+}
+
+export default App;
